6d0831040fb5bbf1848aaf9f52e83a62b99de70a
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-02-10 22:24:29 -0500

    All basic elements of webpage done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 document.getElementById("viewResumeBtn").addEventListener("click", function () {
   window.open("Sheraj_KC_Resume.pdf", "_blank");
 });
+
+// Add JavaScript to trigger animations on scroll
+const sections = document.querySelectorAll(".fade-in");
+
+function checkScroll() {
+  sections.forEach((section) => {
+    const windowHeight = window.innerHeight;
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight * 0.75) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkScroll);
+window.addEventListener("resize", checkScroll);
+checkScroll();
